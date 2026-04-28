aef2fc4ac821def325f0db5b117c973e9d70c768
𝒥𝑜ã𝑜𝐿 <joaolucas.miltec@gmail.com>
2026-04-28 17:50:32 +0000

    fff

--- Aula sei laaaaaaaaaaaaaa quall/a.py
+++ Aula sei laaaaaaaaaaaaaa quall/a.py
@@ -7,12 +7,12 @@ class Retangulo:
     def set_base(self, v):
         if v >= 0: self.__b = v
         else: raise ValueError()
-    def set_base(self, v):
+    def set_altura(self, v):
         if v >= 0: self.__h = v
         else: raise ValueError()
     def get_base(self):
         return self.__b
-    def get_base(self):
+    def get_altura(self):
         return self.__h
     def calc_area(self):
         return self.__b * self.__h
---
+++ equcoes/q1.py
@@ -0,0 +1,9 @@
+class viagem:
+    def __init__(self, d, ds, l):
+        self.__d = ''
+        self.__ds = 0
+        self.__l = 0
+        self.set_d(d)
+        self.set_ds(ds)
+        self.set_l(l)
+
